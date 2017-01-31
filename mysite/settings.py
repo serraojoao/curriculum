01a625c2ffb731c1bf51393032c07ae95c01f14a
Joao Serrao <joaoserrao@MacBook-Pro.home>
2017-01-31 21:40:33 +0100

    changed to db

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,7 +92,7 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASE_URL['default'] =  dj_database_url.config()
 
 # DATABASES = {
 #     #'default': {
